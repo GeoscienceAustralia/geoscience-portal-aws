17f1b109cf0cdd7bc5078cdac52437d87d4c20c7
Lazar Bodor <lazar.bodor@ga.gov.au>
2015-08-16 14:05:59 +1000

    Set webserver hostname to portal-dev.

--- webserver-init.sh
+++ webserver-init.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -x
+echo "portal-dev" > /etc/hostname
 yum -y install tomcat telnet wget
 (cd /usr/share/tomcat/webapps; wget http://internode.dl.sourceforge.net/project/geonetwork/GeoNetwork_opensource/v3.0.1/geonetwork.war; chown tomcat.tomcat geonetwork.war)
 systemctl enable tomcat
