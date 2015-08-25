4ce9437fce0a760a6462fc754f19dd082f5d382b
Lazar Bodor <lazar.bodor@ga.gov.au>
2015-08-25 17:40:50 +1000

    Use cfn-hup to redeploy geoscience-portal.war.

--- stack.py
+++ stack.py
@@ -1,16 +1,27 @@
-# pylint: disable=missing-docstring, invalid-name
+# pylint: disable=missing-docstring, invalid-name, line-too-long
 
 """Geoscience Portal AWS CloudFormation Stack Definition
 """
 
-from troposphere import Base64, Ref, Tags, Template
+import sys
+from troposphere import Base64, Join, Ref, Tags, Template
 from troposphere import cloudformation as cf
 import troposphere.ec2 as ec2
+# import troposphere.iam as iam
+from subprocess import call
+import glob
+import os
+import boto
 
 REDHAT_IMAGEID = "ami-d3daace9"
 SYSTEM_PREFIX = "GeosciencePortal2"
 KEY_PAIR_NAME = "lazar@work"
 WEBSERVER_IP = "54.153.211.253"
+GA_PUBLIC_NEXUS = "http://maven-int.ga.gov.au/nexus/service/local/artifact/maven/redirect?r=public"
+
+stack_id = Ref("AWS::StackId")
+stack_name = Ref("AWS:StackName")
+region = Ref("AWS::Region")
 
 def resource_name(name):
     return SYSTEM_PREFIX + name
@@ -19,8 +30,15 @@ def stack():
     template = Template()
     security_group = template.add_resource(webserver_security_group())
     webserver = template.add_resource(make_webserver(security_group))
+    # webserver_instance_profile = template.add_resource(iam.InstanceProfile(
+    #     "InstanceProfile",
+    #     Path="/",
+    #     Roles=["DeveloperAdmin"],
+    # ))
+    # webserver.IamInstanceProfile = Ref(webserver_instance_profile)
     assign_eip(template, webserver, WEBSERVER_IP)
     return template
+
 
 def assign_eip(template, instance, ip):
     return template.add_resource(ec2.EIPAssociation(
@@ -28,6 +46,20 @@ def assign_eip(template, instance, ip):
         EIP=ip,
         InstanceId=Ref(instance)
     ))
+
+def geoscience_portal_version():
+    return sys.argv[1]
+
+def get_geoscience_portal_war_url():
+    arg = GA_PUBLIC_NEXUS + '&g=au.gov.ga&a=geoscience-portal&v=' + geoscience_portal_version() + '&e=war'
+    call(["wget", arg, '--content-disposition', "--timestamping"])
+    war_filename = max(glob.iglob("*.war"), key=os.path.getctime)
+    call(["aws", "s3", "cp", war_filename, "s3://ga-gov-au/mvn-snapshot/", "--quiet", "--acl", "public-read"])
+    s3 = boto.connect_s3()
+    bucket = s3.get_bucket("ga-gov-au")
+    key = bucket.get_key("mvn-snapshot/" + war_filename)
+    return key.generate_url(3600)
+    # return "http://s3-ap-southeast-2.amazonaws.com/ga.gov.au/mvn-snapshot/" + war_filename
 
 def make_webserver(security_group):
     name = resource_name("WebServer")
@@ -48,23 +80,62 @@ def make_webserver(security_group):
                         "yum": {
                             "telnet": [],
                             "tomcat": [],
+                            "java-1.7.0-openjdk.x86_64": [],
                             "wget": [],
                         }
                     },
-                    files={
-                        "/usr/share/tomcat/webapps/geonetwork.war": {
-                            "source": "http://internode.dl.sourceforge.net/project/geonetwork/GeoNetwork_opensource/v3.0.1/geonetwork.war",
-                            "owner": "tomcat",
-                            "group": "tomcat",
-                        },
-                    },
+                    files=cf.InitFiles({
+                        "/etc/cloud/cloud.cfg.d/99_hostname.cfg": cf.InitFile(
+                            content="#cloud-config\nhostname: portal-dev"
+                        ),
+                        "/usr/share/tomcat/webapps/geonetwork.war": cf.InitFile(
+                            source="http://internode.dl.sourceforge.net/project/geonetwork/GeoNetwork_opensource/v3.0.1/geonetwork.war",
+                            owner="tomcat",
+                            group="tomcat",
+                        ),
+                        "/usr/share/tomcat/webapps/geoscience-portal.war": cf.InitFile(
+                            source=get_geoscience_portal_war_url(),
+                            owner="tomcat",
+                            group="tomcat",
+                        ),
+                        "/etc/cfn/cfn-hup.conf": cf.InitFile(
+                            content=Join('', [
+                                "[main]\n",
+                                "stack=", stack_id, "\n",
+                                "region=", region, "\n",
+                            ]),
+                            mode="000400",
+                            owner="root",
+                            group="root"
+                        ),
+                        "/etc/cfn/hooks.d/cfn-auto-reloader.conf": cf.InitFile(
+                            content=Join("", [
+                                "[cfn-auto-reloader-hook]\n",
+                                "triggers=post.update\n",
+                                "path=Resources.", name, ".Metadata.AWS::CloudFormation::Init\n",
+                                "action=/usr/bin/cfn-init",
+                                " --stack ", stack_id,
+                                " --resource ", name,
+                                " --region ", region, "\n",
+                                "runas=root\n"
+                            ]),
+                        ),
+                    }),
                     services={
-                        "sysvinit": {
-                            "tomcat": {
-                                "enabled": "true",
-                                "ensureRunning": "true",
-                            }
-                        }
+                        "sysvinit": cf.InitServices({
+                            "tomcat": cf.InitService(
+                                enabled=True,
+                                ensureRunning=True,
+                            ),
+                            "cfn-hup": cf.InitService(
+                                enabled=True,
+                                ensureRunning=True,
+                                files=[
+                                    "/etc/cfn/cfn-hup.conf",
+                                    "/etc/cfn/hooks.d/cfn-auto-reloader.conf"
+                                ],
+                            ),
+                        }),
                     }
                 )
             )
--- webserver-init.sh
+++ webserver-init.sh
@@ -1,6 +1,12 @@
-#!/bin/bash
-set -x
-echo "portal-dev" > /etc/hostname
-easy_install https://s3.amazonaws.com/cloudformation-examples/aws-cfn-bootstrap-latest.tar.gz
-cfn-init --region ap-southeast-2 -s GeosciencePortal2 -r GeosciencePortal2WebServer
-# yum -y install tomcat telnet wget
+#cloud-config
+
+hostname: portal-dev
+
+runcmd:
+ - hostnamectl set-hostname portal-dev
+ - easy_install https://s3.amazonaws.com/cloudformation-examples/aws-cfn-bootstrap-latest.tar.gz
+ - ln -s /usr/lib/python2.7/site-packages/aws_cfn_bootstrap-1.4-py2.7.egg/init/redhat/cfn-hup /etc/init.d/cfn-hup
+ - chmod 755 /usr/lib/python2.7/site-packages/aws_cfn_bootstrap-1.4-py2.7.egg/init/redhat/cfn-hup
+ - mkdir -p /opt/aws/bin
+ - ln -s /usr/bin/cfn-hup /opt/aws/bin/cfn-hup
+ - cfn-init --region ap-southeast-2 -s GeosciencePortal2 -r GeosciencePortal2WebServer
