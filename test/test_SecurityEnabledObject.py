7279b37c3c6eebc75517c0b9656d5e3aa05d1364
Shane Nayler <snayler0@hotmail.com>
2016-03-23 10:48:53 +1100

    first test kind of complete

--- test/test_SecurityEnabledObject.py
+++ test/test_SecurityEnabledObject.py
@@ -5,19 +5,26 @@ from troposphere import ec2
 def test_security_enabled_object():
     myvpc = ec2.VPC('myVpc', CidrBlock='10.0.0.0/16')
     myobj = SecurityEnabledObject(title="Unit01Web" ,vpc=myvpc)
-    assert_equals(myobj.security_group)
 
-def test_create_sg()
-    asd
+    assert_equals(myobj.title, "Unit01Web")
+    assert_equals(myobj.security_group.title, "Unit01WebSG")
+
+
+def test_create_sg():
+    # TODO test
+    pass
 
 
 def test_add_flow():
-    asd
+    # TODO test
+    pass
 
 
 def test_add_ingress():
-    asd
+    # TODO test
+    pass
 
 
 def test_add_egress():
-    asd
+    # TODO test
+    pass
