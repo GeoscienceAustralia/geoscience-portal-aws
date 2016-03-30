2fbbefb5c29ec8025c0280cb0df2138edd437225
Fahad Talib <fuddstar@gmail.com>
2016-03-30 14:04:00 +1100

    renaming systests

--- test/sys_tests/test_sys_elb.py
+++ test/sys_tests/test_sys_elb.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 from amazonia.classes.elb import Elb
-from troposphere import ec2, Ref
+from troposphere import ec2, Ref, Template
 
 
 def main():
@@ -9,9 +9,9 @@ def main():
     vpc = ec2.VPC('MyVPC',
                   CidrBlock='10.0.0.0/16')
     pub_sub_list = [ec2.Subnet('MyPubSub1',
-                        AvailabilityZone='ap-southeast-2a',
-                        VpcId=Ref(vpc),
-                        CidrBlock='10.0.1.0/24'),
+                               AvailabilityZone='ap-southeast-2a',
+                               VpcId=Ref(vpc),
+                               CidrBlock='10.0.1.0/24'),
                     ec2.Subnet('MyPubSub2',
                                AvailabilityZone='ap-southeast-2b',
                                VpcId=Ref(vpc),
@@ -19,17 +19,18 @@ def main():
                     ec2.Subnet('MyPubSub3',
                                AvailabilityZone='ap-southeast-2c',
                                VpcId=Ref(vpc),
-                               CidrBlock='10.0.3.0/24')
-                    ]
+                               CidrBlock='10.0.3.0/24')]
 
-    template = Elb(title='elb',
-                   port='80',
-                   subnets=pub_sub_list,
-                   protocol='t2.nano')
+    elb = Elb(title='elb',
+              port='80',
+              subnets=pub_sub_list,
+              protocol='http',
+              stack=Template(),
+              vpc=vpc)
 
-    template.add_resource(vpc)
-    template.add_resource(subnet)
-    print(template.to_json(indent=2, separators=(',', ': ')))
+    elb.stack.add_resource(vpc)
+    elb.stack.add_resource(pub_sub_list)
+    print(elb.stack.to_json(indent=2, separators=(',', ': ')))
 
 if __name__ == "__main__":
     main()
