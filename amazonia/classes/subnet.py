716cd0ad822108e718c84c3f814f94212ba1b0a4
Fahad Talib <fuddstar@gmail.com>
2016-04-01 15:16:00 +1100

    adding public/private determination based on match of route table against stack.route_table, rewrote sub_cidr to use pub/pri boolean to determine sub_cidrs for public and private subnets and create 3rd octect from length of stacks sub_list

--- amazonia/classes/subnet.py
+++ amazonia/classes/subnet.py
@@ -11,46 +11,43 @@ class Subnet(object):
         :param az - Availability Zone
         """
         super(Subnet, self).__init__()
-        vpc = kwargs['vpc']
-        stack = kwargs['stack']
 
-        sub_num = self.num(kwargs['cidr'])
-        subnet_title = 'subnet{0}'.format(sub_num + 1)
+        stack = kwargs['stack']
+        pub_or_pri = 'Public' if kwargs['route_table'] == stack.pub_route_table else 'Private'
+        subnet_title = '{0}Subnet{1}'.format(pub_or_pri, kwargs['az'][-1:].upper())
+
         self.subnet = stack.add_resource(ec2.Subnet(subnet_title,
                                                     AvailabilityZone=kwargs['az'],
                                                     VpcId=kwargs['vpc'],
-                                                    CidrBlock=self.sub_cidr(kwargs['vpc'].CidrBlock,
-                                                                            kwargs['route_table'].title),
+                                                    CidrBlock=self.sub_cidr(stack, kwargs['vpc'].CidrBlock,
+                                                                            pub_or_pri),
                                                     Tags=Tags(Name=Join("",
                                                                         [Ref('AWS::StackName'),
                                                                          '-', subnet_title]))))
 
         """ Create route table associations
         """
-        self.add_associate_route_table(stack, self.subnet, kwargs['route_table'])
+        self.rt_association = self.add_associate_route_table(stack, self.subnet, kwargs['route_table'])
 
     @staticmethod
-    def num(cidr):
-        """ Function to extract the 3rd octet from the Subnet cidr to determine availability zone and name subnets
-        """
-        cidr_split = cidr.split('.')
-
-        return cidr_split[2]
-
-    @staticmethod
-    def sub_cidr(vpc_cidr, route_table_title):
+    def sub_cidr(stack, vpc_cidr, pub_or_pri):
         """
         Function to help create Class C subnet CIDRs from Class A VPC CIDRs
+        :param stack: Stack template
         :param vpc_cidr: VPC CIDR to be broken down into subnets e.g. 10.0.0.0/8
-        :param num: Number of the Subnet
+        :param pub_or_pri: boolean for public or provate subnet determined by route table
         :return:
         """
-        # TODO test Subnet CIDR returns correctly from vpc CIDR fo rmultiple subnets
-        vpc_split = vpc_cidr.split('.')
-        vpc_split[2] = num
-        vpc_last = vpc_split[3].split('/')
-        vpc_last[1] = '24'
-        vpc_split[3] = '/'.join(vpc_last)
+        # 3rd Octect: Obtain length of pub or pri subnet list
+        octect_3 = len(stack.pub_sub_list) if pub_or_pri == 'Public' else len(stack.pri_sub_list)
+        # 2nd Octect: set to '0' for public subnets, set to '1' for private subnets
+        octect_2 = '0' if pub_or_pri == 'Public' else '1'
+        vpc_split = vpc_cidr.split('.')     # separate VPC CIDR for renaming
+        vpc_split[1] = octect_2             # Set 2nd octect based on length of subnet list
+        vpc_split[2] = octect_3             # set 3rd octect based on public or private
+        vpc_last = vpc_split[3].split('/')  # split last group to change subnet mask
+        vpc_last[1] = '24'                  # set subnet mask
+        vpc_split[3] = '/'.join(vpc_last)   # join last group for subnet mask
 
         return '.'.join(vpc_split)
 
