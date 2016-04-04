6d6619a59d57c41e268927fb5d99e2b042f837c2
Fahad Talib <fuddstar@gmail.com>
2016-04-04 13:54:38 +1000

    adding multiple subnets to systems test

--- test/sys_tests/test_sys_subnets.py
+++ test/sys_tests/test_sys_subnets.py
@@ -6,6 +6,7 @@ from troposphere import ec2, Ref, Template
 
 def main():
     stack = Template()
+    az_a = 'ap-southeast-2a'
     az_b = 'ap-southeast-2b'
     az_c = 'ap-southeast-2c'
     stack.pri_sub_list = ['PrivateSubnetA']
@@ -17,12 +18,24 @@ def main():
                                                               VpcId=Ref(stack.vpc)))
     stack.pri_route_table = stack.add_resource(ec2.RouteTable('MyUnitPrivateRouteTable',
                                                               VpcId=Ref(stack.vpc)))
+    pubsubnet1 = Subnet(stack=stack,
+                        route_table=stack.pub_route_table,
+                        az=az_a)
+    pubsubnet2 = Subnet(stack=stack,
+                        route_table=stack.pub_route_table,
+                        az=az_b)
     pubsubnet3 = Subnet(stack=stack,
                         route_table=stack.pub_route_table,
                         az=az_c)
+    prisubnet1 = Subnet(stack=stack,
+                        route_table=stack.pri_route_table,
+                        az=az_a)
     prisubnet2 = Subnet(stack=stack,
                         route_table=stack.pri_route_table,
                         az=az_b)
+    prisubnet3 = Subnet(stack=stack,
+                        route_table=stack.pri_route_table,
+                        az=az_c)
 
     print(stack.to_json(indent=2, separators=(',', ': ')))
 if __name__ == "__main__":
