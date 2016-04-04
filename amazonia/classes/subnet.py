caec4ff539a63ee9cf5ab715045abce917794ba3
Fahad Talib <fuddstar@gmail.com>
2016-04-04 12:29:04 +1000

    Changes to 2nd octect, 3rd octect now calculates based on pub/pri and adds 100 to len, created function to create unit test for subnet, added comments to tests, added test len_az

--- amazonia/classes/subnet.py
+++ amazonia/classes/subnet.py
@@ -8,7 +8,10 @@ class Subnet(object):
         Class to create subnets and associate a route table to it
         AWS CloudFormation - http://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-resource-ec2-subnet.html
         Troposphere - https://github.com/cloudtools/troposphere/blob/master/troposphere/ec2.py
-        :param stack: Stack template object
+        :param stack: Stack template object with required:
+                      * VPC w/ CIDR block,
+                      * Public and private route tables,
+                      * Initialised public and private subnet lists
         :param route_table: Public or private route table object from stack
         :param az: Availability zone where the subnet will be deployed
         """
@@ -44,11 +47,8 @@ class Subnet(object):
         :return: Subnet CIDR based on Public or Private and previous subnets created e.g. 10.1.2.0/24 or 10.0.1.0/24
         """
         # 3rd Octect: Obtain length of pub or pri subnet list
-        octect_3 = len(stack.pub_sub_list) if pub_or_pri == 'Public' else len(stack.pri_sub_list)
-        # 2nd Octect: set to '0' for public subnets, set to '1' for private subnets
-        octect_2 = '0' if pub_or_pri == 'Public' else '1'
+        octect_3 = len(stack.pub_sub_list) if pub_or_pri == 'Public' else len(stack.pri_sub_list) + 100
         vpc_split = stack.vpc.CidrBlock.split('.')      # separate VPC CIDR for renaming
-        vpc_split[1] = octect_2                         # Set 2nd octect based on length of subnet list
         vpc_split[2] = str(octect_3)                    # set 3rd octect based on public or private
         vpc_last = vpc_split[3].split('/')              # split last group to change subnet mask
         vpc_last[1] = '24'                              # set subnet mask
--- test/unit_tests/test_subnets.py
+++ test/unit_tests/test_subnets.py
@@ -2,19 +2,32 @@
 
 from nose.tools import *
 from troposphere import Template, Ref
-from amazonia.classes.elb import Elb
+from amazonia.classes.subnet import Subnet
 
 
 def test_pub_or_pri():
+    """ Validate that public/private subnet is correctly identified from route_table
+    """
 
 
 def test_subnet_title():
+    """ Validate that subnet title is correctly created base don inputs
+    """
 
 
 def test_sub_cidr():
+    """ Validate that subnet CIDR is correctly created for private/public subnets, number of subnet
+    """
 
 
 def test_add_associate_route_table():
+    """ Validate route association created
+    """
+
+
+def test_az_num():
+    """ Validate number of subnets for a Single, Dual and Triple AZ senario
+    """
 
 
 def create_subnets(**kwargs):
@@ -22,6 +35,15 @@ def create_subnets(**kwargs):
     Helper function to create subnet objects.
     :return: Troposphere object for subnet,
     """
-    vpc = 'vpc-12345'
-    subnet = ''
+
+    stack = Template()
+    stack.vpc = 'vpc-12345'
+    stack.pub_route_table = 'MyUnitPublicRouteTable'
+    stack.pri_route_table = 'MyUnitPrivateRouteTable'
+    stack.pri_sub_list = stack.pub_sub_list = []
+    az = ['ap-southeast-2a', 'ap-southeast-2b', 'ap-southeast-2c']
+
+    subnet = Subnet(az=kwargs.get('az', az),
+                    stack=stack,
+                    route_table=kwargs.get('route_table', stack.pub_route_table))
     return subnet
