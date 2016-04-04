a21248e9028c54e9c05bcba008bfdc36331986c9
Fahad Talib <fuddstar@gmail.com>
2016-04-04 14:21:25 +1000

    creating subnets by appending ot list so counter will work properly, list is initialised spearately

--- test/sys_tests/test_sys_subnets.py
+++ test/sys_tests/test_sys_subnets.py
@@ -9,8 +9,8 @@ def main():
     az_a = 'ap-southeast-2a'
     az_b = 'ap-southeast-2b'
     az_c = 'ap-southeast-2c'
-    stack.pri_sub_list = ['PrivateSubnetA']
-    stack.pub_sub_list = ['PublicSubnetA', 'PublicSubnetA']
+    stack.pri_sub_list = []
+    stack.pub_sub_list = []
 
     stack.vpc = stack.add_resource(ec2.VPC('MyVPC',
                                            CidrBlock='10.0.0.0/16'))
@@ -18,24 +18,25 @@ def main():
                                                               VpcId=Ref(stack.vpc)))
     stack.pri_route_table = stack.add_resource(ec2.RouteTable('MyUnitPrivateRouteTable',
                                                               VpcId=Ref(stack.vpc)))
-    pubsubnet1 = Subnet(stack=stack,
+
+    pubsubnet1 = stack.pub_sub_list.append(Subnet(stack=stack,
                         route_table=stack.pub_route_table,
-                        az=az_a)
-    pubsubnet2 = Subnet(stack=stack,
+                        az=az_a))
+    pubsubnet2 = stack.pub_sub_list.append(Subnet(stack=stack,
                         route_table=stack.pub_route_table,
-                        az=az_b)
-    pubsubnet3 = Subnet(stack=stack,
+                        az=az_b))
+    pubsubnet3 = stack.pub_sub_list.append(Subnet(stack=stack,
                         route_table=stack.pub_route_table,
-                        az=az_c)
-    prisubnet1 = Subnet(stack=stack,
+                        az=az_c))
+    prisubnet1 = stack.pri_sub_list.append(Subnet(stack=stack,
                         route_table=stack.pri_route_table,
-                        az=az_a)
-    prisubnet2 = Subnet(stack=stack,
+                        az=az_a))
+    prisubnet2 = stack.pri_sub_list.append(Subnet(stack=stack,
                         route_table=stack.pri_route_table,
-                        az=az_b)
-    prisubnet3 = Subnet(stack=stack,
+                        az=az_b))
+    prisubnet3 = stack.pri_sub_list.append(Subnet(stack=stack,
                         route_table=stack.pri_route_table,
-                        az=az_c)
+                        az=az_c))
 
     print(stack.to_json(indent=2, separators=(',', ': ')))
 if __name__ == "__main__":
