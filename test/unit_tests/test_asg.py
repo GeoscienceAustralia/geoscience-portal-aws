78a67186980ac9d070668a35a8b1bb0afa5d9244
David Gavin <david.gavin@ga.gov.au>
2016-04-05 16:46:39 +1000

    Converting to list comprehension as per Fudd's suggestio

--- test/unit_tests/test_asg.py
+++ test/unit_tests/test_asg.py
@@ -56,8 +56,7 @@ def test_asg():
         assert_equals(asg.asg.title, title + 'Asg')
         assert_equals(asg.asg.MinSize, 1)
         assert_equals(asg.asg.MaxSize, 1)
-        for subnet_id in asg.asg.VPCZoneIdentifier:
-            assert_is(type(subnet_id), Ref)
+        [assert_is(type(subnet_id), Ref) for subnet_id in asg.asg.VPCZoneIdentifier]
         assert_equals(asg.asg.AvailabilityZones, ['ap-southeast-2a'])
         assert_equals(asg.asg.LoadBalancerNames, ['testElb'])
         assert_equals(asg.asg.HealthCheckType, 'ELB')
@@ -66,8 +65,7 @@ def test_asg():
         assert_equals(asg.lc.ImageId, 'ami-893f53b3')
         assert_equals(asg.lc.InstanceType, 't2.nano')
         assert_equals(asg.lc.KeyName, 'pipeline')
-        for sg in asg.lc.SecurityGroups:
-            assert_is(type(sg), Ref)
+        [assert_is(type(sg), Ref) for sg in asg.lc.SecurityGroups]
         assert_equals(asg.cd_app.title, title + 'Asg' + 'Cda')
         assert_equals(asg.cd_app.ApplicationName, title + 'Asg')
         assert_equals(asg.cd_deploygroup.title, title + 'Asg' + 'Cdg')
