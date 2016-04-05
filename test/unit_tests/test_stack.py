0f47e12fb9b8ebd01713932d4b2c66c8cf903c43
David Gavin <david.gavin@ga.gov.au>
2016-04-05 13:41:37 +1000

    Finished initial stack unit test

--- test/unit_tests/test_stack.py
+++ test/unit_tests/test_stack.py
@@ -2,11 +2,13 @@ from nose.tools import *
 
 from amazonia.classes.stack import Stack
 
-userdata = availability_zones = keypair = image_id = instance_type = None
+userdata = availability_zones = keypair = image_id = instance_type = code_deploy_service_role = vpc_cidr = \
+    port = protocol = desiredsize = path2ping = None
 
 
 def setup_resources():
-    global userdata, availability_zones, keypair, image_id, instance_type
+    global userdata, availability_zones, keypair, image_id, instance_type, code_deploy_service_role, vpc_cidr, \
+        port, protocol, desiredsize, path2ping
     userdata = """
     #cloud-config
     repo_update: true
@@ -20,38 +22,62 @@ def setup_resources():
         """
     availability_zones = ['ap-southeast-2a', 'ap-southeast-2b', 'ap-southeast-2c']
     keypair = 'pipeline'
-    image_id = 'ami-893f53b3',
-    instance_type = 't2.nano',
+    image_id = 'ami-893f53b3'
+    instance_type = 't2.nano'
+    code_deploy_service_role = 'instance-iam-role-InstanceProfile-OGL42SZSIQRK'
+    vpc_cidr = '10.0.0.0/16'
+    port = '80'
+    protocol = 'HTTP'
+    desiredsize = 1
+    path2ping = 'HTTP:8080/error/noindex.html'
 
 
 @with_setup(setup_resources())
 def test_stack():
     title = 'app'
     stack = create_stack(title=title)
+    assert_equals(stack.title, title)
 
-
-def create_stack(**kwargs):
-    global userdata, availability_zones, keypair, image_id, instance_type
-    stack = Stack(
-        availability_zones=availability_zones,
-        keypair=keypair,
-        image_id=image_id,
-        instance_type=instance_type,
-        userdata=userdata
-    )
-    return stack
-
-@with_setup(setup_resources)
-def test_sub_cidr():
-    """ Validate that subnet CIDR is correctly created for private/public subnets for number of availablity zones
-    """
-    for num in range(len(az)):
+    for num in range(len(availability_zones)):
         # For public subnets
-        helper_pub_subnet = Subnet.sub_cidr(stack, 'Public')
-        stack.pub_sub_list.append(helper_pub_subnet)
-        assert_equals(helper_pub_subnet, ''.join(['10.0.', str(num), '.0/24']))
+        public_subnet = stack.public_subnets[num]
+        assert_equals(public_subnet.CidrBlock, ''.join(['10.0.', str(num), '.0/24']))
 
         # For private subnets
-        helper_pri_subnet = Subnet.sub_cidr(stack, 'Private')
-        stack.pri_sub_list.append(helper_pri_subnet)
-        assert_equals(helper_pri_subnet, ''.join(['10.0.', str(num + 100), '.0/24']))
+        private_subnet = stack.private_subnets[num]
+        assert_equals(private_subnet.CidrBlock, ''.join(['10.0.', str(num + 100), '.0/24']))
+
+
+def create_stack(title):
+    global userdata, availability_zones, keypair, image_id, instance_type, code_deploy_service_role, vpc_cidr, port, \
+        protocol, desiredsize, path2ping
+    stack = Stack(
+        title=title,
+        code_deploy_service_role=code_deploy_service_role,
+        keypair=keypair,
+        availability_zones=availability_zones,
+        vpc_cidr=vpc_cidr,
+        jump_image_id=image_id,
+        jump_instance_type=instance_type,
+        nat_image_id=image_id,
+        nat_instance_type=instance_type,
+        units=[{'title': 'app1',
+                'protocol': protocol,
+                'port': port,
+                'path2ping': path2ping,
+                'minsize': desiredsize,
+                'maxsize': desiredsize,
+                'image_id': image_id,
+                'instance_type': instance_type,
+                'userdata': userdata},
+               {'title': 'app2',
+                'protocol': protocol,
+                'port': port,
+                'path2ping': path2ping,
+                'minsize': desiredsize,
+                'maxsize': desiredsize,
+                'image_id': image_id,
+                'instance_type': instance_type,
+                'userdata': userdata}],
+    )
+    return stack
