99603feecaf36172732a450027270ae1ca58704e
Fahad Talib <fuddstar@gmail.com>
2016-04-07 14:34:23 +1000

    stubbijng yaml_default and yaml_load files

---
+++ amazonia/classes/yaml_defaults.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+
+# import yaml
+
+"""
+GA's default values to be used for YAML ingestion and overwritten by user values
+"""
+
+
+userdata = """
+#cloud-config
+repo_update: true
+
+packages:
+ - httpd
+
+runcmd:
+ - service httpd start
+"""
+
+image_id = 'ami-893f53b3'
+instance_type = 't2.nano'
+stack_title = 'testStack',
+code_deploy_service_role = 'instance-iam-role-InstanceProfile-OGL42SZSIQRK',
+keypair = 'pipeline',
+availability_zones = ['ap-southeast-2a', 'ap-southeast-2b', 'ap-southeast-2c'],
+vpc_cidr = '10.0.0.0/16',
+jump_image_id = image_id,
+jump_instance_type = instance_type,
+nat_image_id = image_id,
+nat_instance_type = instance_type,
+home_cidr = [('GA1', '124.47.132.132/32'), ('GA2', '192.104.44.0/22')],
+unit_title ='app1',
+protocol = 'HTTP',
+port = '80',
+path2ping = 'index.html',
+minsize = 1,
+maxsize = 1
+
