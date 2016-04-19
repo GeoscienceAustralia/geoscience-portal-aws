7bb845532084c2cf72d7777802843adc5b17dffe
Shane Nayler <snayler0@hotmail.com>
2016-04-19 10:18:34 +1000

    Instance, not isntance (spelling)

--- test/unit_tests/test_single_instance.py
+++ test/unit_tests/test_single_instance.py
@@ -7,7 +7,7 @@ from amazonia.classes.single_instance import SingleInstance
 
 def test_nat_single_instance():
     """
-    Tests for creation of a nat isntance and expects natting (SourceDestCheck)
+    Tests for creation of a nat instance and expects natting (SourceDestCheck)
     to be enabled and the PrivateIp to be displayed in the output.
     :return: Pass
     """
