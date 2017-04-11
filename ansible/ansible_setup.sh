7106b3f2dd4b98bfde8f952649f8ba104a12e830
michaelsexton <michael.sexton@ga.gov.au>
2017-04-11 15:06:51 +1000

    Remove mkdir

--- ansible/ansible_setup.sh
+++ ansible/ansible_setup.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-
 export ANSIBLE_DIR=/etc/ansible
-
-mkdir $ANSIBLE_DIR
 
 wget https://raw.githubusercontent.com/ansible/ansible/devel/contrib/inventory/ec2.py $ANSIBLE_DIR
 wget https://raw.githubusercontent.com/ansible/ansible/devel/contrib/inventory/ec2.py $ANSIBLE_DIR
