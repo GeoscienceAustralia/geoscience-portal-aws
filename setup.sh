1941cc04d59d5ed7de24f0a54448f3545c934739
michaelsexton <michael.sexton@ga.gov.au>
2017-04-11 16:45:56 +1000

    Remove python3

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ export STACK_NAME=$APP_NAME'Dev' # $APP_NAME$ENVIRONMENT
 export JUMPBOX='tag_Name_'$STACK_NAME'_jumpbox'
 
 
-usermod -aG staff ubuntu
+usermod -aG staff $USER
 
 cp ./ansible/geoscience-portal-rsa  ~/.ssh/
 chmod 600 ~/.ssh/geoscience-portal-rsa
