24a6fc83a3f2acd142fc90c8fdc4e5c788ff8724
michaelsexton <michael.sexton@ga.gov.au>
2017-04-11 15:04:33 +1000

    Change to setup.sh

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+export APP_NAME=GeosciencePortal
+export STACK_NAME=$APP_NAME'Dev' # $APP_NAME$ENVIRONMENT
+#export JUMPBOX=$STACK_NAME'_jumpbox'
+export JUMPBOX='tag_Name_'$STACK_NAME'_jumpbox'
+
+
+cp ./ansible/geoscience-portal-rsa  ~/.ssh/
+chmod 600 ~/.ssh/geoscience-portal-rsa
+
+eval `ssh-agent`
+ssh-add ~/.ssh/geoscience-portal-rsa
