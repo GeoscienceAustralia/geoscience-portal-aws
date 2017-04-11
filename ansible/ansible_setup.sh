459845eb1c731e3510da906f30da8bc5395a0ede
michaelsexton <michael.sexton@ga.gov.au>
2017-04-11 15:14:27 +1000

    Add sudo

--- ansible/ansible_setup.sh
+++ ansible/ansible_setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 export ANSIBLE_DIR=/etc/ansible
 
-wget https://raw.githubusercontent.com/ansible/ansible/devel/contrib/inventory/ec2.py $ANSIBLE_DIR
-wget https://raw.githubusercontent.com/ansible/ansible/devel/contrib/inventory/ec2.ini $ANSIBLE_DIR
+sudo wget https://raw.githubusercontent.com/ansible/ansible/devel/contrib/inventory/ec2.py $ANSIBLE_DIR
+sudo wget https://raw.githubusercontent.com/ansible/ansible/devel/contrib/inventory/ec2.ini $ANSIBLE_DIR
 
 export ANSIBLE_HOSTS=$ANSIBLE_DIR/ec2.py
 export EC2_INI_PATH=$ANSIBLE_DIR/ec2.ini
