8f943f0020ce0b36c7cabecf756aa7b9fd36c0c0
michaelsexton <michael.sexton@ga.gov.au>
2017-04-11 10:45:42 +1000

    Add environment variables

--- ansible/ansible_setup.sh
+++ ansible/ansible_setup.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-$ANSIBLE_DIR = /etc/ansible
+export ANSIBLE_DIR = /etc/ansible
 
 mkdir $ANSIBLE_DIR
 
 wget https://raw.githubusercontent.com/ansible/ansible/devel/contrib/inventory/ec2.py $ANSIBLE_DIR
 cp ec2.ini $ANSIBLE_DIR
+
+export ANSIBLE_HOSTS=$ANSIBLE_DIR/ec2.py
+export EC2_INI_PATH=$ANSIBLE_DIR/ec2.ini
